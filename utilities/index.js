084660a038712ff09720a9d6a983cfda4e19da68
AzureFirehawk <madison.thomas094@gmail.com>
2025-09-19 08:48:07 -0500

    vehicle display HTML

--- utilities/index.js
+++ utilities/index.js
@@ -58,6 +58,28 @@ Util.buildClassificationGrid = async function(data) {
 }
 
 /* ***************************************
+ * Build the vehicle view HTML
+ * ************************************ */
+Util.buildVehicle = function (data) {
+    let vehicle
+    if (data) {
+        vehicle += '<div id="vehicle-display">';
+        vehicle += `<img src="${data.inv_image}" alt="Image of ${data.inv_make} ${data.inv_model} on CSE Motors">`;
+        vehicle += '<div class="vehicle-details">';
+        vehicle += `<h2>${data.inv_year} ${data.inv_make} ${data.inv_model}</h2>`;
+        vehicle += `<p><strong>Price:</strong> $${new Intl.NumberFormat("en-US").format(data.inv_price)}</p>`;
+        vehicle += `<p><strong>Miles:</strong> ${new Intl.NumberFormat("en-US").format(data.inv_miles)}</p>`;
+        vehicle += `<p><strong>Color:</strong> ${data.inv_color}</p>`;
+        vehicle += `<p>${data.inv_description}</p>`;
+        vehicle += '</div>';
+        vehicle += '</div>';
+    } else {
+        vehicle += '<p class="notice">Sorry, no matching vehicle could be found.</p>'
+    }
+    return vehicle;
+}
+
+/* ***************************************
  * Middleware for Handling Errors
  * Wrap other function in this for
  * General Error Handling
