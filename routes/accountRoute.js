1b1e906dd2f8e2bc36eb7f35aa355e46f9d56427
AzureFirehawk <madison.thomas094@gmail.com>
2025-09-23 13:07:36 -0500

    registration submit adjusted to 'post' action and added routing for post

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,10 +4,13 @@ const router = new express.Router();
 const utilities = require("../utilities/");
 const accountController = require("../controllers/accountController");
 
-//  Route to build account view
+// Route to build account view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
-//  Route to build register view
+// Route to build register view
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
+
+// Post register submission
+router.post("/register", utilities.handleErrors(accountController.registerAccount));
 
 module.exports = router;
