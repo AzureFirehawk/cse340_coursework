db68bf85fe7cd1dcdf9add69d310bd588a8f5fa0
AzureFirehawk <madison.thomas094@gmail.com>
2025-09-23 15:41:33 -0500

    update post route to include verification

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities/");
+const regValidate = require("../utilities/account-validation");
 const accountController = require("../controllers/accountController");
 
 // Route to build account view
@@ -11,6 +12,11 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
 // Post register submission
-router.post("/register", utilities.handleErrors(accountController.registerAccount));
+router.post(
+    "/register",
+    regValidate.registrationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+);
 
 module.exports = router;
