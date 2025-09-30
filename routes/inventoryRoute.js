932acfee1d60547b0b9bc84b2594c168a32116bc
AzureFirehawk <madison.thomas094@gmail.com>
2025-09-30 18:30:57 -0500

    error handling fix for

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -6,10 +6,10 @@ const utilities = require("../utilities/");
 const invController = require("../controllers/invController");
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 
 // Route to build inventory by inventory_id view
-router.get("/detail/:inv_id", invController.buildByInventoryId);
+router.get("/detail/:inv_id", utilities.handleErrors(invController.buildByInventoryId));
 
 // Route to build the inventory management view
 router.get("/", invController.buildManagement);
