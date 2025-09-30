f7c5fcb411b6af362d9826744ab23e30885a09b3
AzureFirehawk <madison.thomas094@gmail.com>
2025-09-30 14:30:40 -0500

    get inventory data as JSON

--- controllers/invController.js
+++ controllers/invController.js
@@ -34,6 +34,20 @@ invCont.buildByInventoryId = async function (req, res, next) {
 }
 
 /* *****************************
+ * Return inventory by Classification as JSON
+ * ***************************** */
+invCont.getInventoryJSON = async (req, res, next) => {
+    const classification_id = parseInt(req.params.classification_id);
+    const invData = await invModel.getInventoryByClassificationId(classification_id);
+    if (invData[0].inv_id) {
+        return res.json(invData)
+    } else {
+        next(new Error("No data returned"))
+    }
+}
+
+
+/* *****************************
  * Build management view
  * ***************************** */
 invCont.buildManagement = async function (req, res, next) {
