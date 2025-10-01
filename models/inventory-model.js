5bcaf4acf8570f3231246acf359936a2ac90b872
AzureFirehawk <madison.thomas094@gmail.com>
2025-10-01 14:27:11 -0500

    Delete vehicle query and export

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -131,5 +131,27 @@ async function updateVehicle(
     }
 }
 
+/* ****************************
+ * Delete vehicle data
+ * ************************** */
+async function deleteVehicle(inv_id) {
+    try {
+        const sql = "DELETE FROM inventory WHERE inv_id = $1 RETURNING *";
+        const data = await pool.query(sql, [inv_id]);
+        return data;
+    } catch (error) {
+        new Error("Delete Inventory Error");
+    }
+}
 
-module.exports = { getClassifications, getInventoryByClassificationId, getVehicleById, addClassification, checkExistingClass, addVehicle, checkExistingVehicle, updateVehicle };
+module.exports = {
+    getClassifications,
+    getInventoryByClassificationId,
+    getVehicleById,
+    addClassification,
+    checkExistingClass,
+    addVehicle,
+    checkExistingVehicle,
+    updateVehicle,
+    deleteVehicle
+};
