4b7ed2a3a1e475d9d14ea9429c81e6c56cc3c5bb
AzureFirehawk <madison.thomas094@gmail.com>
2025-10-10 12:00:00 -0500

    formatting and labeling

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -73,7 +73,11 @@ async function checkExistingClass(classification_name) {
  * ************************** */
 async function addVehicle(inv_make, inv_model, inv_year, inv_price, inv_image, inv_thumbnail, inv_description, inv_miles, inv_color, classification_id) {
     try {
-        const sql = "INSERT INTO inventory (inv_make, inv_model, inv_year, inv_price, inv_image, inv_thumbnail, inv_description, inv_miles, inv_color, classification_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *";
+        const sql = `
+            INSERT INTO inventory (inv_make, inv_model, inv_year, inv_price, inv_image, inv_thumbnail, inv_description, inv_miles, inv_color, classification_id)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+            RETURNING *;
+        `;
         return await pool.query(sql, [inv_make, inv_model, inv_year, inv_price, inv_image, inv_thumbnail, inv_description, inv_miles, inv_color, classification_id]);
     } catch (error) {
         return error.message;
@@ -110,8 +114,21 @@ async function updateVehicle(
     classification_id
 ) {
     try {
-        const sql = 
-            "UPDATE public.inventory SET inv_make = $1, inv_model = $2, inv_description = $3, inv_image = $4, inv_thumbnail = $5, inv_price = $6, inv_year = $7, inv_miles = $8, inv_color = $9, classification_id = $10 WHERE inv_id = $11 RETURNING *";
+        const sql = `
+            UPDATE public.inventory SET 
+                inv_make = $1, 
+                inv_model = $2, 
+                inv_description = $3, 
+                inv_image = $4, 
+                inv_thumbnail = $5, 
+                inv_price = $6, 
+                inv_year = $7, 
+                inv_miles = $8, 
+                inv_color = $9, 
+                classification_id = $10 
+            WHERE inv_id = $11 
+            RETURNING *;
+        `;
         const data = await pool.query(sql, [
             inv_make,
             inv_model,
@@ -136,13 +153,21 @@ async function updateVehicle(
  * ************************** */
 async function deleteVehicle(inv_id) {
     try {
-        const sql = "DELETE FROM inventory WHERE inv_id = $1 RETURNING *";
+        const sql = `
+            DELETE FROM inventory
+            WHERE inv_id = $1
+            RETURNING *
+        `;
         const data = await pool.query(sql, [inv_id]);
         return data;
     } catch (error) {
         new Error("Delete Inventory Error");
     }
-}
+} 
+
+/* ****************************
+ * Exports
+ * ************************** */
 
 module.exports = {
     getClassifications,
