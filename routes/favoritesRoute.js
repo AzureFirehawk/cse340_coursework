60449bf057f85f3be26a592733cb034e53fdd55c
AzureFirehawk <madison.thomas094@gmail.com>
2025-10-10 12:25:28 -0500

    favorites routes creation

---
+++ routes/favoritesRoute.js
@@ -0,0 +1,16 @@
+// Needed resources
+const express = require("express");
+const router = new express.Router();
+const utilities = require("../utilities/");
+const favCont = require("../controllers/favoritesController");
+
+// Add favorite
+router.post("/add", utilities.checkLogin, utilities.handleErrors(favCont.addFavorite));
+
+// Remove favorite
+router.post("/remove", utilities.checkLogin, utilities.handleErrors(favCont.removeFavorite));
+
+// Favorites page
+router.get("/", utilities.checkLogin, utilities.handleErrors(favCont.buildFavorites));
+
+module.exports = router;
